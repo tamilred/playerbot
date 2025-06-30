6bf7f545ec2bb54437143495a01324b4fdc42c83
tamilred <128729255+tamilred@users.noreply.github.com>
2025-06-30 10:32:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_ID = 12345678
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
-ADMIN_ID = 123456789
+API_ID = 23794727
+API_HASH = "35140d8eefc686d2c60da1fb622dcdde"
+BOT_TOKEN = "7862056115:AAHQkuUBz9s84BKvIaxud3xsv41CKB3jpDY"
+ADMIN_ID = 7299955528
 PAYMENT_PROVIDER_TOKEN = "381764678:TEST:2d3e58dd3bc748e4bc6fecc8d9e041a0"
-MONGO_URI = "mongodb+srv://username:password@cluster.mongodb.net/audio_bot?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://itstamilhad:hdxeAJmN0IMJR2QX@playerbot.tso38ud.mongodb.net/?retryWrites=true&w=majority&appName=PlayerBot"
